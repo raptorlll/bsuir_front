8d003641710e853e6da73e092330de592dbe6edb
Pavel Leonov <p.leonov@itransition.com>
2018-02-06 00:22:56 +0300

    Before moving to asynck task

--- app/src/main/java/com/timbuchalka/top10downloader/fragment/consultantinformation/ConsultantInformationUpdateFragment.java
+++ app/src/main/java/com/timbuchalka/top10downloader/fragment/consultantinformation/ConsultantInformationUpdateFragment.java
@@ -32,7 +32,17 @@ import com.timbuchalka.top10downloader.models.ConsultantGroupUser;
 import com.timbuchalka.top10downloader.models.ConsultantInformation;
 import com.timbuchalka.top10downloader.models.User;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -44,8 +54,7 @@ import java.util.Locale;
 //https://stackoverflow.com/questions/34276466/simple-httpurlconnection-post-file-multipart-form-data-from-android-to-google-bl
 public class ConsultantInformationUpdateFragment
         extends UpdateFragment<ConsultantInformation>
-        implements ListData.OnDataAvailable<ConsultantGroupUser>
-{
+        implements ListData.OnDataAvailable<ConsultantGroupUser> {
     private TextView education;
     private TextView degree;
     private TextView licenseNumber;
@@ -59,7 +68,7 @@ public class ConsultantInformationUpdateFragment
 
 
     @SuppressLint("ValidFragment")
-    ConsultantInformationUpdateFragment(){
+    ConsultantInformationUpdateFragment() {
         super();
     }
 
@@ -67,10 +76,10 @@ public class ConsultantInformationUpdateFragment
     public void onDataAvailable(Collection<ConsultantGroupUser> data, DownloadStatus status) {
         System.out.println("");
 
-        consultantGroupUserData = (ArrayList<ConsultantGroupUser>)data;
+        consultantGroupUserData = (ArrayList<ConsultantGroupUser>) data;
         ArrayList<String> list = new ArrayList<String>();
 
-        for (ConsultantGroupUser u : data){
+        for (ConsultantGroupUser u : data) {
             list.add(u.getUser().getFirstName().concat(" ").concat(u.getUser().getEmail()));
         }
 
@@ -82,8 +91,8 @@ public class ConsultantInformationUpdateFragment
             consultantGroupUser.setSelection(0);
         } else {
             int i = 0;
-            for (ConsultantGroupUser u : data){
-                if(activeElement.getConsultantGroupUser().getId() == u.getId()){
+            for (ConsultantGroupUser u : data) {
+                if (activeElement.getConsultantGroupUser().getId() == u.getId()) {
                     consultantGroupUser.setSelection(i);
                 }
 
@@ -94,7 +103,7 @@ public class ConsultantInformationUpdateFragment
         consultantGroupUser.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                activeElement.setConsultantGroupUser(((ArrayList<ConsultantGroupUser>)consultantGroupUserData).get(i));
+                activeElement.setConsultantGroupUser(((ArrayList<ConsultantGroupUser>) consultantGroupUserData).get(i));
                 System.out.println("");
             }
 
@@ -126,13 +135,44 @@ public class ConsultantInformationUpdateFragment
         if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
             // Use the provided utility method to parse the result
             List<Uri> files = Utils.getSelectedFilesFromResult(intent);
-            for (Uri uri: files) {
+            for (Uri uri : files) {
                 File file = Utils.getFileForUri(uri);
                 fileChoosed = Utils.getFileForUri(uri);
-                // Do something with the result...
+
+                MultipartUtility multipart = null;
+                try {
+                    multipart = new MultipartUtility("http://10.0.2.2:8080/consultant_information/save", "UTF-8");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                multipart.addFormField("data", "{\n" +
+                        "\t\"education\": \"MIT\",\n" +
+                        "\t\"degree\": \"PhD\",\n" +
+                        "\t\"licenseNumber\": \"License by NY Government\",\n" +
+                        "\t\"licenseFile\": \"file.png\",\n" +
+                        "\t\"licenseUntil\": \"2017-12-30\",\n" +
+                        "\t\"availableFrom\": \"09:00:00\",\n" +
+                        "\t\"availableUntil\": \"16:00:00\",\n" +
+                        "\t\"consultantGroupUser\": 2\n" +
+                        "}");
+
+                try {
+                    multipart.addFilePart("file",fileChoosed);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                List<String> response = null;
+                try {
+                    response = multipart.finish();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
+
     @Override
     public void convertForView(ConsultantInformation activeElement) {
         education.setText(activeElement.getEducation().toString());
@@ -152,9 +192,9 @@ public class ConsultantInformationUpdateFragment
 
     @Override
     public void onClickListeners(View view) {
-        if(view == licenseUntil) {
+        if (view == licenseUntil) {
             datePicker.show();
-        } else if (view == licenseFile){
+        } else if (view == licenseFile) {
             initFilePicker();
         }
     }
@@ -170,11 +210,11 @@ public class ConsultantInformationUpdateFragment
                 Calendar mcurrentTime = Calendar.getInstance();
 
                 int hour = activeElement.getAvailableFrom() != null ?
-                        Integer.parseInt((new SimpleDateFormat("H", Locale.US)).format(activeElement.getAvailableFrom())):
+                        Integer.parseInt((new SimpleDateFormat("H", Locale.US)).format(activeElement.getAvailableFrom())) :
                         mcurrentTime.get(Calendar.HOUR_OF_DAY);
 
                 int minute = activeElement.getAvailableFrom() != null ?
-                        Integer.parseInt((new SimpleDateFormat("m", Locale.US)).format(activeElement.getAvailableFrom())):
+                        Integer.parseInt((new SimpleDateFormat("m", Locale.US)).format(activeElement.getAvailableFrom())) :
                         mcurrentTime.get(Calendar.MINUTE);
 
                 TimePickerDialog mTimePicker;
@@ -198,11 +238,11 @@ public class ConsultantInformationUpdateFragment
                 Calendar mcurrentTime = Calendar.getInstance();
 
                 int hour = activeElement.getAvailableUntil() != null ?
-                        Integer.parseInt((new SimpleDateFormat("H", Locale.US)).format(activeElement.getAvailableUntil())):
+                        Integer.parseInt((new SimpleDateFormat("H", Locale.US)).format(activeElement.getAvailableUntil())) :
                         mcurrentTime.get(Calendar.HOUR_OF_DAY);
 
                 int minute = activeElement.getAvailableUntil() != null ?
-                        Integer.parseInt((new SimpleDateFormat("m", Locale.US)).format(activeElement.getAvailableUntil())):
+                        Integer.parseInt((new SimpleDateFormat("m", Locale.US)).format(activeElement.getAvailableUntil())) :
                         mcurrentTime.get(Calendar.MINUTE);
 
                 TimePickerDialog mTimePicker;
@@ -244,7 +284,7 @@ public class ConsultantInformationUpdateFragment
         consultantGroupUser = (Spinner) v.findViewById(R.id.consultantGroupUser);
     }
 
-    private void initFilePicker(){
+    private void initFilePicker() {
         Intent i = new Intent(getContext(), FilePickerActivity.class);
         // This works if you defined the intent filter
         // Intent i = new Intent(Intent.ACTION_GET_CONTENT);
@@ -266,5 +306,133 @@ public class ConsultantInformationUpdateFragment
     @Override
     public Fragment getListView() {
         return new ConsultantInformationFragment();
+    }
+
+    public class MultipartUtility {
+        private final String boundary;
+        private static final String LINE_FEED = "\r\n";
+        private HttpURLConnection httpConn;
+        private String charset;
+        private OutputStream outputStream;
+        private PrintWriter writer;
+
+        /**
+         * This constructor initializes a new HTTP POST request with content type
+         * is set to multipart/form-data
+         *
+         * @param requestURL
+         * @param charset
+         * @throws IOException
+         */
+        public MultipartUtility(String requestURL, String charset)
+                throws IOException {
+            this.charset = charset;
+
+            // creates a unique boundary based on time stamp
+            boundary = "===" + System.currentTimeMillis() + "===";
+            URL url = new URL(requestURL);
+            httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setUseCaches(false);
+            httpConn.setDoOutput(true);    // indicates POST method
+            httpConn.setDoInput(true);
+            httpConn.setRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=" + boundary);
+            outputStream = httpConn.getOutputStream();
+            writer = new PrintWriter(new OutputStreamWriter(outputStream, charset),
+                    true);
+        }
+
+        /**
+         * Adds a form field to the request
+         *
+         * @param name  field name
+         * @param value field value
+         */
+        public void addFormField(String name, String value) {
+            writer.append("--" + boundary).append(LINE_FEED);
+            writer.append("Content-Disposition: form-data; name=\"" + name + "\"")
+                    .append(LINE_FEED);
+            writer.append("Content-Type: text/plain; charset=" + charset).append(
+                    LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.append(value).append(LINE_FEED);
+            writer.flush();
+        }
+
+        /**
+         * Adds a upload file section to the request
+         *
+         * @param fieldName  name attribute in <input type="file" name="..." />
+         * @param uploadFile a File to be uploaded
+         * @throws IOException
+         */
+        public void addFilePart(String fieldName, File uploadFile)
+                throws IOException {
+            String fileName = uploadFile.getName();
+            writer.append("--" + boundary).append(LINE_FEED);
+            writer.append(
+                    "Content-Disposition: form-data; name=\"" + fieldName
+                            + "\"; filename=\"" + fileName + "\"")
+                    .append(LINE_FEED);
+            writer.append(
+                    "Content-Type: "
+                            + URLConnection.guessContentTypeFromName(fileName))
+                    .append(LINE_FEED);
+            writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.flush();
+
+            FileInputStream inputStream = new FileInputStream(uploadFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            writer.append(LINE_FEED);
+            writer.flush();
+        }
+
+        /**
+         * Adds a header field to the request.
+         *
+         * @param name  - name of the header field
+         * @param value - value of the header field
+         */
+        public void addHeaderField(String name, String value) {
+            writer.append(name + ": " + value).append(LINE_FEED);
+            writer.flush();
+        }
+
+        /**
+         * Completes the request and receives response from the server.
+         *
+         * @return a list of Strings as response in case the server returned
+         * status OK, otherwise an exception is thrown.
+         * @throws IOException
+         */
+        public List<String> finish() throws IOException {
+            List<String> response = new ArrayList<String>();
+            writer.append(LINE_FEED).flush();
+            writer.append("--" + boundary + "--").append(LINE_FEED);
+            writer.close();
+
+            // checks server's status code first
+            int status = httpConn.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        httpConn.getInputStream()));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    response.add(line);
+                }
+                reader.close();
+                httpConn.disconnect();
+            } else {
+                throw new IOException("Server returned non-OK status: " + status);
+            }
+            return response;
+        }
     }
 }
